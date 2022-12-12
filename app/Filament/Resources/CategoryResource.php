c4879f8cc6f292dc3892ebce2326607bcb28de0a
abdulwhab241 <abdulwhab241@gmail.com>
2022-12-11 23:29:53 -0500

    fix pages

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Category;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\CategoryResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-list';
+    protected static ?string $activeNavigationIcon = 'heroicon-s-document-text';
+    protected static ?string $navigationGroup = 'إدارة الاقسام';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Card::make()
+            ->schema([
+                TextInput::make('name')
+                ->label('الأسم:')
+                ->required()
+                ->maxLength(255)
+        ])
+            
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id') -> sortable(),
+                TextColumn::make('name')->label('الأسم')->sortable() -> searchable(),
+                TextColumn::make('created_at')->label('تاريخ الإنشاء')->dateTime()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }    
+}
